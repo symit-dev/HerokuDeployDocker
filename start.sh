8642a4c2992e208dc13637b3bac2e670f45a2aba
symit-dev <76552519+symit-dev@users.noreply.github.com>
2025-02-25 22:15:29 +0530

    fix start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,14 @@
+#!/bin/bash
+
 rm -rf temp_repo
 git clone $GIT_URL temp_repo
-mv temp_repo/* temp_repo/.* .
+
+# Use rsync instead of mv to avoid "Device or resource busy" errors
+rsync -a temp_repo/ . --remove-source-files
 rm -rf temp_repo
 
+# Ensure run.sh has execution permissions
+chmod +x run.sh
+
+# Execute run.sh
 ./run.sh
