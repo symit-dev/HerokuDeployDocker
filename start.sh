4e3d7badf0f97c466f2b8d8353bfd46d5f783ab8
symit-dev <76552519+symit-dev@users.noreply.github.com>
2025-02-25 22:16:09 +0530

    move instead of rsycn

--- start.sh
+++ start.sh
@@ -3,8 +3,8 @@
 rm -rf temp_repo
 git clone $GIT_URL temp_repo
 
-# Use rsync instead of mv to avoid "Device or resource busy" errors
-rsync -a temp_repo/ . --remove-source-files
+# Use cp instead of mv to avoid "Device or resource busy" errors
+cp -r temp_repo/. .
 rm -rf temp_repo
 
 # Ensure run.sh has execution permissions
